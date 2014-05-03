4af47e6cbe744c44998336628c9028d219013d1e
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2014-05-03 12:49:06 +0000

    refactoring tests to use metaclass behavior, so each file that is saved is its own test (to better indicate where problems a problem is encountered)
    Review: http://codecollaborator/ui#review:id=3170
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/imageutils/": change = 778465]

--- python/imageutils/test/test_saving.py
+++ python/imageutils/test/test_saving.py
@@ -1,32 +1,33 @@
-__author__ = 'filipp'
-
-import unittest
-import imageutils
-from imageutils.test import load_image, get_image_paths
-import tempfile
 import os
 import shutil
+import tempfile
+import unittest
+
+import testhelpers
+
+import imageutils
+from .. import test
 
 
 def _get_relative_path(path):
     return os.path.relpath(path, os.path.dirname(__file__))
 
 
-class TestSaving(unittest.TestCase):
+class TestSavingBase(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
 
-    def _check_same_images(self, image1, image2):
+    def check_same_images(self, image1, image2):
         self.assertEquals(image2.type, image1.type)
         self.assertEquals(image2.format, image1.format)
         self.assertEquals(image2.width, image1.width)
         self.assertEquals(image2.height, image1.height)
         self.assertEquals(image2.mip_count, image1.mip_count)
 
-    def _save_temp_image_with_extension(self, bmp, filename, extension):
+    def save_temp_image_with_extension(self, bmp, filename, extension):
         out_name = os.path.join(self.temp_dir, filename)
         try:
             os.makedirs(os.path.dirname(out_name))
@@ -36,18 +37,28 @@ class TestSaving(unittest.TestCase):
         bmp.save(out_name)
         return out_name
 
-    def test_can_save_image_to_dds(self):
-        for filename in get_image_paths():
-            bmp = load_image(filename)
-            rel_name = _get_relative_path(filename)
-            out_name = self._save_temp_image_with_extension(bmp, rel_name, '.dds')
-            out = imageutils.load(out_name)
-            self._check_same_images(bmp, out)
 
-    def test_can_save_image_to_png(self):
-        for filename in get_image_paths('uncompressed'):
-            bmp = load_image(filename)
-            rel_name = _get_relative_path(filename)
-            out_name = self._save_temp_image_with_extension(bmp, rel_name, '.png')
-            out = imageutils.load(out_name)
-            self._check_same_images(bmp, out)
+class TestSaving(TestSavingBase):
+    __metaclass__ = testhelpers.TestGeneratorMetaClass
+    __metatestitems__ = [(os.path.basename(fp), fp)
+                         for fp in test.get_image_paths()]
+
+    def _test_can_save_image_to_dds(self, filename):
+        bmp = test.load_image(filename)
+        rel_name = _get_relative_path(filename)
+        out_name = self.save_temp_image_with_extension(bmp, rel_name, '.dds')
+        out = imageutils.load(out_name)
+        self.check_same_images(bmp, out)
+
+
+class TestSavingUncompressed(TestSavingBase):
+    __metaclass__ = testhelpers.TestGeneratorMetaClass
+    __metatestitems__ = [(os.path.basename(fp), fp)
+                         for fp in test.get_image_paths('uncompressed')]
+
+    def _test_can_save_image_to_png(self, filename):
+        bmp = test.load_image(filename)
+        rel_name = _get_relative_path(filename)
+        out_name = self.save_temp_image_with_extension(bmp, rel_name, '.png')
+        out = imageutils.load(out_name)
+        self.check_same_images(bmp, out)
