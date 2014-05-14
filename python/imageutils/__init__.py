a63cbd40376be77a26e89ee4f6dca0edc01c0615
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2014-05-14 20:21:38 +0000

    Fixing imageutils to not do its own binbootstrapping. Also fixing surrounding packages that relied on imageutils doing its own thing, now each package has to declare that dependency.
    Review: http://codecollaborator/ui#review:id=3283
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/imageutils/": change = 783871]

--- python/imageutils/__init__.py
+++ python/imageutils/__init__.py
@@ -1,16 +1,10 @@
-__author__ = 'filipp'
-
-import binbootstrapper
-app_root = binbootstrapper.get_approot()
-if app_root is None:
-    app_root = __file__
-binbootstrapper.update_binaries(app_root, binbootstrapper.DLL_IMAGETOOLS)
-del app_root
-
-from _imagetools import *
 from PIL import Image as PILImage
-import osutils
 import struct
+
+import osutils
+
+# noinspection PyUnresolvedReferences
+from _imagetools import *
 
 
 def from_pil(pil_image):
--- python/imageutils/test/test_compression.py
+++ python/imageutils/test/test_compression.py
@@ -1,10 +1,28 @@
-__author__ = 'filipp'
-
-import unittest
-import imageutils
-from imageutils.test import load_image, get_image_paths
 import itertools
 import os
+import unittest
+
+import binbootstrapper
+binbootstrapper.update_binaries(__file__, binbootstrapper.DLL_IMAGETOOLS)
+
+import imageutils
+
+
+RESOURCE_DIR = os.path.join(os.path.dirname(__file__), 'res')
+
+
+def load_image(name):
+    return imageutils.load(get_image_path(name))
+
+
+def get_image_path(name):
+    return os.path.join(RESOURCE_DIR, name)
+
+
+def get_image_paths(subdir=''):
+    for root, _, files in os.walk(os.path.join(RESOURCE_DIR, subdir)):
+        for name in files:
+            yield os.path.join(root, name)
 
 
 def is_compressed_image(filename):
